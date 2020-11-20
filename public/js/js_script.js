544db9af9196d8b58c93e0ca25f2568db2cde019
jullemusten <hellstrom.julia@hotmail.com>
2020-11-20 23:47:18 +0100

    Taking away comments

--- public/js/js_script.js
+++ public/js/js_script.js
@@ -1,47 +1,3 @@
-/*var buttonClicked = function() {
-
-  radios = document.getElementsByName('gender');
-  console.log(radios)
-  for (i=0; i<radios.length; i++)
-  if (radios[i].checked) {
-    gender=radios[i].id;
-  };
-
-  let burgerOrder=[];
-  let boxList = document.getElementsByName('burgerCheckbox');
-  for (j=0; j<boxList.length; j++) 
-    if (boxList[j].checked) {
-      burgerOrder+=boxList[j].value;
-    };
-
-    console.log(burgerOrder);
-    let summaryText = "Order confirmation";
-    let customerText = "Customer details";
-    let orderText = "Order summary";
-
-    let fullnameOrder = (document.getElementById('fullname').value);
-    let emailOrder = (document.getElementById('email').value);
-    let recipientOrder = (document.getElementById('recipient').value)
-
-    clicked = true;
-
-
-    /*array=[console.log(document.getElementById('fullname').value), console.log(document.getElementById('email').value), console.log(document.getElementById('street').value), console.log(document.getElementById('house').value), console.log(document.getElementById('recipient').value), console.log(gender)];*/
-    /*infoAboutOrder=[fullnameOrder, emailOrder, recipientOrder, gender, burgerOrder];
-    infoAboutOrder=[fullnameOrder, emailOrder, recipientOrder, gender, burgerOrder];
-    console.log(infoAboutOrder);
-  
-  return infoAboutOrder;*/
-    
-
-  /*var orderInformation = [{
-    "fullname": "document.getElementById('fullname').value",
-    "email": "document.getElementById('email').value",
-    "delivery adress": "array[3] + array[4]",
-    "payment": ,
-    "orderBurgers": */
-  /*};*/
-
 var personalInfo = function() {
 
   radios = document.getElementsByName('gender');
@@ -63,31 +19,7 @@ var orderTable = function() {
   let boxList = document.getElementsByName('burgerCheckbox');
   for (j=0; j<boxList.length; j++) 
     if (boxList[j].checked) {
-      /*burgerOrderArray+=boxList[j].value;*/
       burgerOrderArray.push(boxList[j].value);
     };
     return burgerOrderArray;
 };
-
-  /*var orderInformation = function() {
-    radios = document.getElementsByName('gender')
-    console.log(radios)
-  for (i=0; i<radios.length; i++)
-  if (radios[i].checked) {
-    gender=radios[i].id;
-  };
-};
-
-/*var getInfo = function() {
-  console.log(document.getElementById('fullname').value);
-  console.log(document.getElementById('email').value); 
-  console.log(document.getElementById('street').value); 
-  console.log(document.getElementById('house').value); 
-  console.log(document.getElementById('recipient').value); 
-  console.log(customerInfo);
-  radios = document.getElementsByName('gender');
-  for (i=0; i<radios.length; i++)
-  if (radios[i].checked) {
-    gender=radios[i].id;
-  }
-}*/
--- public/js/vue_script.js
+++ public/js/vue_script.js
@@ -1,17 +1,10 @@
 var socket = io();
-/*import * as io from 'socket.io-client';
-window.io=io;*/
 
 var lm = new Vue({
   el: '#website',
   data: {
     title: "Pick a burger ",
     menu: food,
-    /*information: orderInformation,
-    markDone,
-    /*info: orderInformation,*/
-    /*markDone,*/
-    /*infoAboutOrder: [],*/
     personalInfoArray: [],
     burgerOrderArray: [],
     orders: {},
@@ -31,7 +24,6 @@ var lm = new Vue({
 
   methods: {
     orderButton: function() {
-      /*this.infoAboutOrder=buttonClicked(),*/
       this.personalInfoArray=personalInfo(),
       this.burgerOrderArray=orderTable(),
       this.addOrder(),
@@ -46,13 +38,10 @@ var lm = new Vue({
     },
 
     addOrder: function (event) {
-      /*var offset = {x: event.currentTarget.getBoundingClientRect().left,
-                    y: event.currentTarget.getBoundingClientRect().top};*/
       socket.emit("addOrder", { 
         orderId: this.getNext(),
         coord: this.coord,
         orders: this.orders,
-        orderItems: this.infoAboutOrder, /*Vill ha burger order*/
         personalItems: this.personalInfoArray,
         orderItems: this.burgerOrderArray,                                
         });
@@ -67,40 +56,9 @@ var lm = new Vue({
           x: event.clientX - 10 - locationMap.x,
           y: event.clientY - 10 - locationMap.y
         }
-        /*this.coord.x = event.clientX - 10 - locationMap.x;
-        this.coord.y = event.clientY - 10 - locationMap.y;*/
      },
 
-    /*addOrder: function (event) {
-      socket.emit("addOrder", {
-        orderId: this.getNext(),
-        details: {
-          x: event.clientX - 10 - event.currentTarget.getBoundingClientRect().left,
-          y: event.clientY - 10 - event.currentTarget.getBoundingClientRect().top,
-        },
-        orderItems: ["Beans", "Curry"]
-      });
-    },*/
   }
-  /*methods: {
-    markDone: function(event) {
-      new orderInformation;
-    }
-  }
-  /*methods: {
-    markDone: function() {
-      Flytta hit markDone från JAvaScript, ta bort markdone komandot
-      gör en v-model, gör v-model på alla radiobuttoms, lägg till en variabel i datadleen
-
-      kopplar det till en variabel i data delen
-    radios = document.getElementsByName('gender');
-    for (i=0; i<radios.length; i++)
-    if (radios[i].checked) {
-      gender=radios[i].id;
-    }
-    array=[console.log(document.getElementById('fullname').value), console.log(document.getElementById('email').value), console.log(document.getElementById('street').value), console.log(document.getElementById('house').value), console.log(document.getElementById('recipient').value), console.log(gender)];
-    }  
-  }*/
 
 })
 
